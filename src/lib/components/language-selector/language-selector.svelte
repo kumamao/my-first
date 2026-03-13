de99ec667ee67fca772d4bdf319b9746856dbe9e
zhangyi <zhangyi@way-s.cn>
2026-03-13 10:02:55 +0800

    提交测试

--- src/lib/components/language-selector/language-selector.svelte
+++ src/lib/components/language-selector/language-selector.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
+import * as Select from '$lib/components/ui/select/index.js';
   import { getTranslations } from './index.ts';
 
   const t = getTranslations();
+
+  const translations = [
+	{ value: 'en', label: 'English' },
+	{ value: 'ar', label: 'Arabic (العربية)' },
+	{ value: 'he', label: 'Hebrew (עברית)' }
+];
+
+const triggerContent = $derived(translations.find((f) => f.value === t.language)?.label ?? 'Select a fruit');
 </script>
 
-<div class="h-7x rounded-lg border border-border px-2 py-[3px] text-sm">
+<!-- <div class="h-7x rounded-lg border border-border px-2 py-[3px] text-sm">
   <select class="pr-1 outline-0" bind:value={t.language}>
     <option value="en">English</option>
     <option value="ar">Arabic (العربية)</option>
     <option value="he">Hebrew (עברית)</option>
   </select>
-</div>
+</div> -->
+<Select.Root type="single" name="favoriteFruit" bind:value={t.language}>
+	<Select.Trigger class="w-[180px]">
+	{triggerContent}
+	</Select.Trigger>
+	<Select.Content>
+	<Select.Group>
+		{#each translations as translation (translation.value)}
+		<Select.Item value={translation.value} label={translation.label} disabled={translation.value === 'grapes'}>
+			{translation.label}
+		</Select.Item>
+		{/each}
+	</Select.Group>
+	</Select.Content>
+</Select.Root>
